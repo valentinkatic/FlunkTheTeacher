a1678c49dacd95bcbbe98185269d01d85735273c
Valentin Katić <valentin.katic@gmail.com>
2016-06-09 01:08:49 +0200

    Upload source koda

---
+++ app/src/main/java/com/katic/flunktheteacher/MainActivity.java
@@ -0,0 +1,64 @@
+package com.katic.flunktheteacher;
+
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainView mainView;
+
+    private static final int TOGGLE_SOUND = 1;
+    private boolean soundEnabled = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        mainView = (MainView) findViewById(R.id.head);
+
+        mainView.setKeepScreenOn(true);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem toggleSound = menu.add(0, TOGGLE_SOUND, 0, "Toggle Sound");
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case TOGGLE_SOUND:
+
+                /*When the user toggles the sound option, a toast is displayed that
+                indicates a change; default to a string with the text “Sound On”.
+                If the sound is enabled, you toggle off both the boolean and the
+                text. Otherwise, you toggle both of them on. Then you display the toast.*/
+                String soundEnabledText = "Sound On";
+                if (soundEnabled) {
+                    soundEnabled = false;
+                    mainView.soundOn = false;
+                    soundEnabledText = "Sound Off";
+                } else {
+                    soundEnabled = true;
+                    mainView.soundOn = true;
+                }
+                Toast.makeText(this, soundEnabledText, Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+
+}
